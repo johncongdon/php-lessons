8fdd849b785fd0e76c9322f888248aa30b755a27
John Congdon <john@johncongdon.com>
2012-11-18 11:46:55 -0800

    Starting lesson 01

---
+++ lesson-01/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *  By convention, the initial "Home Page" is called index.php.  It is the file
+ *  that is loaded when someone goes to just the domain name.  Example:
+ *  http://www.sdphp.org/ actually loads http://www.sdphp.org/index.php
+ *
+ */
+
+/* The /* starts a multi line comment.
+ * They will be used extensively throughout these lessons.
+ * They end with */
+
+
